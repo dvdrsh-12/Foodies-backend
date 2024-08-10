d4e5d5a61ef8dfc7c1331bb912681d8c0cf4a764
Devadarsh M R <devadarshmr12567@gmail.com>
2024-08-10 23:55:28 +0530

    Create user modified

--- routes/createUser.js
+++ routes/createUser.js
@@ -26,7 +26,7 @@ router.post("/createUser", [
                 email: req.body.email,
                 location: req.body.location
             });
-            const authToken = jwt.sign(req.body.email, jwtSecret);
+            const authToken = jwt.sign(req.body.email, process.env.JWT_SECRET);
             return res.json({ success: true, authToken: authToken, email: req.body.email });
         } catch (error) {
             console.log("Error occured", error);
@@ -54,7 +54,7 @@ router.post("/loginUser", [
                 return res.status(400).json({ errors: "Incorrect password" });
             }
             const data = userData.email;
-            const authToken = jwt.sign(data, jwtSecret);
+            const authToken = jwt.sign(data, process.env.JWT_SECRET);
             return res.json({ success: true, authToken: authToken, email: email });
         } catch (error) {
             console.log("Error occured", error);
